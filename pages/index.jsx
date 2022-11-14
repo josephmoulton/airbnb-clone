e894bd04bb5a102827dbb84d8b903ca6e2d10e4c
Joe <josephlewismoulton@gmail.com>
2022-11-14 15:24:25 +0000

    banner completed

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header"
+import Banner from "../components/Banner"
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       </Head>
 
       <Header></Header>
+      <Banner></Banner>
     </div>
   );
 };
