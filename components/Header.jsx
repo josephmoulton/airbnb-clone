6fb04dca538b056e0f75c67bdd9435b90eb08a53
Joe <josephlewismoulton@gmail.com>
2022-11-15 15:41:51 +0000

    Calendar functionality added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import {
   MagnifyingGlassIcon,
@@ -7,8 +7,26 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from "@heroicons/react/24/solid";
-
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       <div className="relative flex items-center h-10 cursor-pointer my-auto flex-row">
@@ -21,10 +39,12 @@ function Header() {
       </div>
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-md">
         <input
+          value={searchInput}
           className="pl-5 bg-transparent flex-grow outline-none
           text-sm text-gray-600 placeholder-gray-400"
           type="text"
           placeholder="Start your search"
+          onChange={(e) => setSearchInput(e.target.value)}
         />
         <MagnifyingGlassIcon
           className="hidden
@@ -33,12 +53,45 @@ function Header() {
       </div>
       <div className="flex items-center space-x-4 justify-end text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
-        <GlobeEuropeAfricaIcon className="h-6 cursor-pointer"/>
+        <GlobeEuropeAfricaIcon className="h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-          <Bars3Icon className="h-6 cursor-pointer"/>
-          <UserCircleIcon className="h-6 cursor-pointer"/>
+          <Bars3Icon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-4">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4 pb-2">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={numberOfGuests}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+            />
+          </div>
+          <div className="flex">
+            <button
+              className="flex-grow text-gray-500"
+              onClick={() => setSearchInput("")}
+            >
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
---
+++ pages/search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+        <h1>Im the search page</h1>
+    </div>
+  )
+}
+
+export default Search
